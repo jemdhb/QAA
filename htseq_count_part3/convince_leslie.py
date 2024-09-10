c8dfcc0691220e03900ac6ea3340052ad27c631a
Julia Jones <jujo@login2.talapas.uoregon.edu>
2024-09-09 20:32:56 -0700

    fixed graphs

--- htseq_count_part3/convince_leslie.py
+++ htseq_count_part3/convince_leslie.py
@@ -24,7 +24,7 @@ def get_counts(fh):
     ax.yaxis.set_major_formatter(formatter)
     ax.xaxis.set_major_formatter(formatter)
 
-    ax.set(title=f"{args.filename[:args.filename.index(".")]}: Number of genes with certain counts",ylabel="Number of genes",
+    ax.set(title=f"{args.filename[:args.filename.index(".")]}: Log-scaled number of genes with certain counts",ylabel="Number of genes",
            xlabel="Counts")
     sns.despine(top=True,right=True)
     out_file=args.filename[:args.filename.index(".")]+".png"
@@ -34,17 +34,24 @@ def get_counts(fh):
 def get_categories(fh):
     data=fh.read().split("\n")
 
-    #for our summary categories, create a list of tuples formatted (category, count)
-    cats=[item.split()[0].replace("_"," ").strip() for item in data if item.split("\t")[0].startswith("__")]
-    counts=[int(item.split()[1].strip()) for item in data if item.split("\t")[0].startswith("__")]
-    _,ax=plt.subplots(figsize=(15, 8))
+    #for our summary categories, create a list of counts
+    #tack on mapped
+    cats=[item.split()[0].replace("_"," ").strip() for item in data if item.split("\t")[0].startswith("__")]+["mapped"]
+    
+    #anything that is not a summary stat (__) is mapped content
+    counts=[int(item.split()[1].strip()) for item in data if item.split("\t")[0].startswith("__")]+\
+           [sum([int(item.split()[1].strip()) for item in data if item.split("\t")[0].startswith("ENS")])]
+    _,ax=plt.subplots(figsize=(15,8))
+
+    #barh so use x-scale 
+    plt.xscale("log")
     plt.barh(cats,counts,color="seagreen",alpha=0.7)
     sns.despine(top=True,right=True)
-    ax.set(title=f"{args.filename[:args.filename.index(".")]}: Number of reads per category",ylabel="Counts",
+    ax.set(title=f"{args.filename[:args.filename.index(".")]}: Log-scaled number of reads per category",ylabel="Counts",
            xlabel="Category")
     out_file="cats_"+args.filename[:args.filename.index(".")]+".png"
     plt.savefig(out_file)
 
 
-get_counts(fh)
-#get_categories(fh)
+#get_counts(fh)
+get_categories(fh)
